227bb5fceebf09d9b50ab7555744a253b55ef9e3
y <donhuvy@outlook.com>
2026-06-27 18:17:36 +0700

    font size 2x , post

--- wp-content/themes/bkit/functions.php
+++ wp-content/themes/bkit/functions.php
@@ -57,6 +57,12 @@ if ( ! function_exists( 'bkit_setup' ) ) :
                 'script',
             )
         );
+
+        // Add support for block editor styles.
+        add_theme_support( 'editor-styles' );
+
+        // Enqueue editor styles.
+        add_editor_style( 'style.css' );
     }
 endif;
 add_action( 'after_setup_theme', 'bkit_setup' );

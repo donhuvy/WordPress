67033417d15592a2bb03e4f82d8d3f1e20c60789
y <donhuvy@outlook.com>
2026-07-10 21:35:37 +0700

    Update searchform.php

--- wp-content/themes/bkit/searchform.php
+++ wp-content/themes/bkit/searchform.php
@@ -8,5 +8,11 @@
         <span class="screen-reader-text"><?php echo _x( 'Tìm kiếm cho:', 'label', 'bkit' ); ?></span>
         <input type="search" class="search-field" placeholder="<?php echo esc_attr_x( 'Tìm kiếm bài viết &hellip;', 'placeholder', 'bkit' ); ?>" value="<?php echo get_search_query(); ?>" name="s" required />
     </label>
-    <button type="submit" class="search-submit"><?php echo esc_html_x( 'Tìm kiếm', 'submit button', 'bkit' ); ?></button>
+    <button type="submit" class="search-submit">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="18" height="18" fill="none" stroke="currentColor" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round" class="search-icon">
+            <circle cx="11" cy="11" r="8"></circle>
+            <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
+        </svg>
+        <span class="screen-reader-text"><?php echo esc_html_x( 'Tìm kiếm', 'submit button', 'bkit' ); ?></span>
+    </button>
 </form>

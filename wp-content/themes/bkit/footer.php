1a48db39263d1c258435f07dfc9db4f5bd5e7976
Vy Do <donhuvy@outlook.com>
2026-08-18 05:18:44 +0700

    d

--- wp-content/themes/bkit/footer.php
+++ wp-content/themes/bkit/footer.php
@@ -5,7 +5,7 @@
             $logo_url = get_template_directory_uri() . '/images/bkit.png';
             ?>
             <img src="<?php echo esc_url( $logo_url ); ?>" alt="<?php bloginfo( 'name' ); ?> Logo" class="site-logo" style="height: 35px;">
-            <span class="site-title" style="color: #ff0000 !important; font-weight: 800; font-size: 1.25rem;"><?php bloginfo( 'name' ); ?></span>
+            <span class="site-title"><?php bloginfo( 'name' ); ?></span>
         </div>
         <div class="footer-copyright">
             <p>&copy; <?php echo date( 'Y' ); ?> <?php bloginfo( 'name' ); ?>.</p>
